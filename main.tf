c09732da9e02112e918401901d8e6899766251b7
BigJTheAdmin <javonpri@gmail.com>
2026-08-24 15:42:25 -0400

    Add Terraform configuration for AWS VPC

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  cloud {
+    organization = "PingTraceSSH"
+    workspaces {
+      name = "aws-terraform-network-lab"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "lab-vpc"
+  }
+}
