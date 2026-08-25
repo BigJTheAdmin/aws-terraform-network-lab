626d3e8a6b01c2e735bcae017b6e96fa9d8dccf4
BigJTheAdmin <javonpri@gmail.com>
2026-08-25 09:56:04 -0400

    Add public and private subnets to main.tf

--- main.tf
+++ main.tf
@@ -23,4 +23,23 @@ resource "aws_vpc" "main" {
   tags = {
     Name = "lab-vpc"
   }
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block               = var.public_subnet_cidr
+  availability_zone        = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.name_prefix}-public-subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = var.availability_zone
+
+  tags = {
+    Name = "${var.name_prefix}-private-subnet"
+  }
 }
