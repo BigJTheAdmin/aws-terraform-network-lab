fc0f8a2fcaea79f11c1b72c01dfc8187bb545c5e
BigJTheAdmin <javonpri@gmail.com>
2026-08-25 13:28:53 -0400

    Add outputs for transit gateway and attachments

--- outputs.tf
+++ outputs.tf
@@ -21,3 +21,15 @@ output "vpc_b_public_subnet_id" {
 output "vpc_b_private_subnet_id" {
   value = aws_subnet.private_b.id
 }
+
+output "tgw_id" {
+  value = aws_ec2_transit_gateway.main.id
+}
+
+output "tgw_attachment_a_id" {
+  value = aws_ec2_transit_gateway_vpc_attachment.vpc_a.id
+}
+
+output "tgw_attachment_b_id" {
+  value = aws_ec2_transit_gateway_vpc_attachment.vpc_b.id
+}
