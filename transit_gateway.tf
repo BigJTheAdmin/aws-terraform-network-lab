222552a0dde46273800dec59a067a00e8695eba9
BigJTheAdmin <javonpri@gmail.com>
2026-08-25 13:27:23 -0400

    Add AWS Transit Gateway configuration for VPCs
    This configuration sets up a transit gateway connecting two VPCs, along with their respective VPC attachments and routes.

---
+++ transit_gateway.tf
@@ -0,0 +1,46 @@
+resource "aws_ec2_transit_gateway" "main" {
+  description = "Connects lab VPC A and VPC B"
+  tags = {
+    Name = "${var.name_prefix}-tgw"
+  }
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "vpc_a" {
+  transit_gateway_id = aws_ec2_transit_gateway.main.id
+  vpc_id              = aws_vpc.this["vpc_a"].id
+  subnet_ids          = [aws_subnet.private.id]
+  tags = {
+    Name = "${local.vpc_names["vpc_a"]}-tgw-attach"
+  }
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "vpc_b" {
+  transit_gateway_id = aws_ec2_transit_gateway.main.id
+  vpc_id              = aws_vpc.this["vpc_b"].id
+  subnet_ids          = [aws_subnet.private_b.id]
+  tags = {
+    Name = "${local.vpc_names["vpc_b"]}-tgw-attach"
+  }
+}
+
+resource "aws_route" "a_to_b" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = var.vpcs["vpc_b"]
+  transit_gateway_id     = aws_ec2_transit_gateway.main.id
+
+  depends_on = [
+    aws_ec2_transit_gateway_vpc_attachment.vpc_a,
+    aws_ec2_transit_gateway_vpc_attachment.vpc_b
+  ]
+}
+
+resource "aws_route" "b_to_a" {
+  route_table_id         = aws_route_table.private_b.id
+  destination_cidr_block = var.vpcs["vpc_a"]
+  transit_gateway_id     = aws_ec2_transit_gateway.main.id
+
+  depends_on = [
+    aws_ec2_transit_gateway_vpc_attachment.vpc_a,
+    aws_ec2_transit_gateway_vpc_attachment.vpc_b
+  ]
+}
