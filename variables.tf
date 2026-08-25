d14be4fe8a8f203f1794c9a6368c7d9c8f36f880
BigJTheAdmin <javonpri@gmail.com>
2026-08-25 11:08:27 -0400

    Modify subnet variables and add VPC B configuration
    Updated descriptions for existing subnet variables and added new variables for VPC B's subnets. Introduced a map variable for VPC CIDR blocks.

--- variables.tf
+++ variables.tf
@@ -1,13 +1,25 @@
 variable "public_subnet_cidr" {
-  description = "CIDR block for the public subnet"
+  description = "CIDR block for VPC A's public subnet"
   type        = string
   default     = "10.0.1.0/24"
 }
 
 variable "private_subnet_cidr" {
-  description = "CIDR block for the private subnet"
+  description = "CIDR block for VPC A's private subnet"
   type        = string
   default     = "10.0.2.0/24"
+}
+
+variable "public_subnet_cidr_b" {
+  description = "CIDR block for VPC B's public subnet"
+  type        = string
+  default     = "10.1.1.0/24"
+}
+
+variable "private_subnet_cidr_b" {
+  description = "CIDR block for VPC B's private subnet"
+  type        = string
+  default     = "10.1.2.0/24"
 }
 
 variable "availability_zone" {
@@ -20,4 +32,13 @@ variable "name_prefix" {
   description = "Prefix used for resource naming/tags"
   type        = string
   default     = "lab"
+}
+
+variable "vpcs" {
+  description = "Map of VPC key to CIDR block"
+  type        = map(string)
+  default = {
+    vpc_a = "10.0.0.0/16"
+    vpc_b = "10.1.0.0/16"
+  }
 }
