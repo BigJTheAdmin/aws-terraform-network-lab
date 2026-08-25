2b69dbed71abc904874eace263e078a6d39fec4a
BigJTheAdmin <javonpri@gmail.com>
2026-08-25 11:12:11 -0400

    Refactor VPC output variables for multiple VPCs

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,23 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
+output "vpc_a_id" {
+  value = aws_vpc.this["vpc_a"].id
 }
 
-output "public_subnet_id" {
+output "vpc_b_id" {
+  value = aws_vpc.this["vpc_b"].id
+}
+
+output "vpc_a_public_subnet_id" {
   value = aws_subnet.public.id
 }
 
-output "private_subnet_id" {
+output "vpc_a_private_subnet_id" {
   value = aws_subnet.private.id
 }
 
-output "public_route_table_id" {
-  value = aws_route_table.public.id
+output "vpc_b_public_subnet_id" {
+  value = aws_subnet.public_b.id
 }
 
-output "private_route_table_id" {
-  value = aws_route_table.private.id
+output "vpc_b_private_subnet_id" {
+  value = aws_subnet.private_b.id
 }
